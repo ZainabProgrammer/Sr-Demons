7eb10bc274ce3e889e5d64ac3576ffec4e63efe0
Mac <mac@Macs-MacBook-Pro.local>
2023-10-29 10:58:41 +0500

    --

--- src/pages/sub_category/[title].js
+++ src/pages/sub_category/[title].js
@@ -6,18 +6,18 @@ import AppThemeProvider from "@/theme/AppThemeProvider";
 import { useRouter } from "next/router";
 import { Provider, useSelector } from "react-redux";
 const title = () => {
-  const router = useRouter();
-  const category_title = router.query.title;
-  const category = useSelector((state) => state.category.allData);
-  let singlecat = category.flatMap((e) => e.categories);
-  console.log(
-    singlecat.map((e) => e.sub_category),
-    "dynamic"
-  );
-  let sub_category = singlecat.map((e) => e.sub_category);
-  let sub_category_data = sub_category.find((e) => e.title === category_title);
+  // const router = useRouter();
+  // const category_title = router.query.title;
+  // const category = useSelector((state) => state.category.allData);
+  // let singlecat = category.flatMap((e) => e.categories);
+  // console.log(
+  //   singlecat.map((e) => e.sub_category),
+  //   "dynamic"
+  // );
+  // let sub_category = singlecat.map((e) => e.sub_category);
+  // let sub_category_data = sub_category.find((e) => e.title === category_title);
 
-  console.log(sub_category_data, "sub data");
+  // console.log(sub_category_data, "sub data");
   return (
     <>
       <AppThemeProvider>
